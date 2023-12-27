667d1f9bc44ebc9ad9f644d9b054723abf94ef1f
warihadikusuma <90770633+warihadikusuma@users.noreply.github.com>
2023-12-27 19:48:46 +0700

    Update poli.sql

--- poli.sql
+++ poli.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Dec 26, 2023 at 02:41 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Host: localhost:8111
+-- Generation Time: Dec 27, 2023 at 01:40 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -26,303 +26,72 @@ SET time_zone = "+00:00";
 --
 -- Table structure for table `daftar_poli`
 --
-
-CREATE TABLE `daftar_poli` (
-  `id` int(11) NOT NULL,
-  `id_pasien` int(11) NOT NULL,
-  `id_jadwal` int(11) NOT NULL,
-  `keluhan` text NOT NULL,
-  `no_antrian` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- Error reading structure for table poli.daftar_poli: #1932 - Table 'poli.daftar_poli' doesn't exist in engine
+-- Error reading data for table poli.daftar_poli: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `poli`.`daftar_poli`' at line 1
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `detail_periksa`
 --
-
-CREATE TABLE `detail_periksa` (
-  `id` int(11) UNSIGNED NOT NULL,
-  `id_periksa` int(11) NOT NULL,
-  `id_obat` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- Error reading structure for table poli.detail_periksa: #1932 - Table 'poli.detail_periksa' doesn't exist in engine
+-- Error reading data for table poli.detail_periksa: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `poli`.`detail_periksa`' at line 1
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `dokter`
 --
-
-CREATE TABLE `dokter` (
-  `id` int(11) NOT NULL,
-  `nama` varchar(255) NOT NULL,
-  `alamat` varchar(255) NOT NULL,
-  `no_hp` varchar(50) NOT NULL,
-  `id_poli` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- Error reading structure for table poli.dokter: #1932 - Table 'poli.dokter' doesn't exist in engine
+-- Error reading data for table poli.dokter: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `poli`.`dokter`' at line 1
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `jadwal_periksa`
 --
-
-CREATE TABLE `jadwal_periksa` (
-  `id` int(11) NOT NULL,
-  `id_dokter` int(11) NOT NULL,
-  `hari` enum('senin','selasa','rabu','kamis','jumat','sabtu') NOT NULL,
-  `jam_mulai` time NOT NULL,
-  `jam_selesai` time NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- Error reading structure for table poli.jadwal_periksa: #1932 - Table 'poli.jadwal_periksa' doesn't exist in engine
+-- Error reading data for table poli.jadwal_periksa: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `poli`.`jadwal_periksa`' at line 1
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `obat`
 --
-
-CREATE TABLE `obat` (
-  `id` int(11) NOT NULL,
-  `nama_obat` varchar(50) NOT NULL,
-  `kemasan` varchar(35) NOT NULL,
-  `harga` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `obat`
---
-
-INSERT INTO `obat` (`id`, `nama_obat`, `kemasan`, `harga`) VALUES
-(1, 'paracetamol', 'botol', 10000),
-(2, 'migrain', 'sachet', 3000),
-(3, 'konidin', 'sachet', 5000),
-(4, 'kalpanax', 'sachet', 6000),
-(1451, 'Diapet', 'Botol', 150000),
-(1452, 'Sirplus', 'Botol', 45000);
+-- Error reading structure for table poli.obat: #1932 - Table 'poli.obat' doesn't exist in engine
+-- Error reading data for table poli.obat: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `poli`.`obat`' at line 1
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `pasien`
 --
-
-CREATE TABLE `pasien` (
-  `id` int(11) NOT NULL,
-  `nama` varchar(255) NOT NULL,
-  `alamat` varchar(255) NOT NULL,
-  `no_ktp` varchar(255) NOT NULL,
-  `no_hp` varchar(50) NOT NULL,
-  `no_rm` varchar(25) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- Error reading structure for table poli.pasien: #1932 - Table 'poli.pasien' doesn't exist in engine
+-- Error reading data for table poli.pasien: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `poli`.`pasien`' at line 1
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `periksa`
 --
-
-CREATE TABLE `periksa` (
-  `id` int(11) NOT NULL,
-  `id_daftar_poli` int(11) NOT NULL,
-  `tgl_periksa` datetime NOT NULL,
-  `catatan` text NOT NULL,
-  `biaya_periksa` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- Error reading structure for table poli.periksa: #1932 - Table 'poli.periksa' doesn't exist in engine
+-- Error reading data for table poli.periksa: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `poli`.`periksa`' at line 1
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `poli`
 --
-
-CREATE TABLE `poli` (
-  `id` int(11) NOT NULL,
-  `nama_poli` varchar(25) NOT NULL,
-  `keterangan` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+-- Error reading structure for table poli.poli: #1932 - Table 'poli.poli' doesn't exist in engine
+-- Error reading data for table poli.poli: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `poli`.`poli`' at line 1
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `user_login`
 --
-
-CREATE TABLE `user_login` (
-  `id` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `role` enum('admin','dokter','pasien') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `user_login`
---
-
-INSERT INTO `user_login` (`id`, `username`, `password`, `role`) VALUES
-(1, 'nopal', 'nopal123', 'admin'),
-(4, 'pasien', 'pasien123', 'pasien'),
-(7, 'dokter', 'dokter123', 'dokter'),
-(8, 'richard', 'richard123', 'dokter'),
-(9, 'naufal', 'naufal123', 'pasien');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `daftar_poli`
---
-ALTER TABLE `daftar_poli`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_id_pasien` (`id_pasien`),
-  ADD KEY `fk_id_jp` (`id_jadwal`);
-
---
--- Indexes for table `detail_periksa`
---
-ALTER TABLE `detail_periksa`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_id_obat` (`id_obat`),
-  ADD KEY `fk_id_dp` (`id_periksa`);
-
---
--- Indexes for table `dokter`
---
-ALTER TABLE `dokter`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_id_poli` (`id_poli`);
-
---
--- Indexes for table `jadwal_periksa`
---
-ALTER TABLE `jadwal_periksa`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_id_dokter` (`id_dokter`);
-
---
--- Indexes for table `obat`
---
-ALTER TABLE `obat`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `pasien`
---
-ALTER TABLE `pasien`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `periksa`
---
-ALTER TABLE `periksa`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_id_periksa` (`id_daftar_poli`);
-
---
--- Indexes for table `poli`
---
-ALTER TABLE `poli`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `user_login`
---
-ALTER TABLE `user_login`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `daftar_poli`
---
-ALTER TABLE `daftar_poli`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `detail_periksa`
---
-ALTER TABLE `detail_periksa`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `dokter`
---
-ALTER TABLE `dokter`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `jadwal_periksa`
---
-ALTER TABLE `jadwal_periksa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `obat`
---
-ALTER TABLE `obat`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1454;
-
---
--- AUTO_INCREMENT for table `pasien`
---
-ALTER TABLE `pasien`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `periksa`
---
-ALTER TABLE `periksa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `poli`
---
-ALTER TABLE `poli`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `user_login`
---
-ALTER TABLE `user_login`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `daftar_poli`
---
-ALTER TABLE `daftar_poli`
-  ADD CONSTRAINT `fk_id_jp` FOREIGN KEY (`id_jadwal`) REFERENCES `jadwal_periksa` (`id`),
-  ADD CONSTRAINT `fk_id_pasien` FOREIGN KEY (`id_pasien`) REFERENCES `pasien` (`id`);
-
---
--- Constraints for table `detail_periksa`
---
-ALTER TABLE `detail_periksa`
-  ADD CONSTRAINT `fk_id_dp` FOREIGN KEY (`id_periksa`) REFERENCES `periksa` (`id`),
-  ADD CONSTRAINT `fk_id_obat` FOREIGN KEY (`id_obat`) REFERENCES `obat` (`id`);
-
---
--- Constraints for table `dokter`
---
-ALTER TABLE `dokter`
-  ADD CONSTRAINT `fk_id_poli` FOREIGN KEY (`id_poli`) REFERENCES `poli` (`id`);
-
---
--- Constraints for table `jadwal_periksa`
---
-ALTER TABLE `jadwal_periksa`
-  ADD CONSTRAINT `fk_id_dokter` FOREIGN KEY (`id_dokter`) REFERENCES `dokter` (`id`);
-
---
--- Constraints for table `periksa`
---
-ALTER TABLE `periksa`
-  ADD CONSTRAINT `fk_id_periksa` FOREIGN KEY (`id_daftar_poli`) REFERENCES `daftar_poli` (`id`);
+-- Error reading structure for table poli.user_login: #1932 - Table 'poli.user_login' doesn't exist in engine
+-- Error reading data for table poli.user_login: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `poli`.`user_login`' at line 1
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
